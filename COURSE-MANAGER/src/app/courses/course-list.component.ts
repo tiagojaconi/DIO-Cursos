d6768e6f2a2df4229f67158e7d45fba5df79f4c4
Tiago Jaconi <64618541+tiagojaconi@users.noreply.github.com>
2022-07-07 17:14:06 -0300

    Course Manager, em Angular.

---
+++ COURSE-MANAGER/src/app/courses/course-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+
+import { Course } from "./course";
+import { CourseService } from "./course.service";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html',
+    styleUrls: ['./course-list.component.css']
+})
+export class CourseListComponent implements OnInit {
+
+    constructor (private coursesService: CourseService){}
+    
+    filteredCourses: Course[] = [];
+
+    _courses: Course[] = [];
+
+    _filterBy: string = '';
+
+    ngOnInit(): void{
+        this._courses = this.coursesService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string){
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter() {
+        return this._filterBy;
+    }
+}
